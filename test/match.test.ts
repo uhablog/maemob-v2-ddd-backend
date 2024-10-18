72653999ec9755c30b1f100ab58000bdd079f615
uhablog <uha.blog98@gmail.com>
2024-10-19 08:55:04 +0900

    テスト修正

--- test/match.test.ts
+++ test/match.test.ts
@@ -18,38 +18,37 @@ afterAll(async () => {
 
 
 describe('Match APIのテスト', () => {
-  // it('case1: POST /match - 新しい試合が作成される', async () => {
-  //   const response = await request(app)
-  //     .post('/match')
-  //     .send({
-  //       homePlayer: "Taro",
-  //       awayPlayer: "Hanako",
-  //       homeScore: 1,
-  //       awayScore: 0
-  //     });
+  it('case1: POST /match - 新しい試合が作成される', async () => {
+    const response = await request(app)
+      .post('/match')
+      .send({
+        homePlayer: "Taro",
+        awayPlayer: "Hanako",
+        homeScore: 1,
+        awayScore: 0
+      });
 
-  //   expect(response.status).toBe(201);
-  //   expect(response.body).toHaveProperty('id');
-  //   expect(response.body.homePlayer).toBe('Taro');
-  //   expect(response.body.awayPlayer).toBe('Hanako');
-  //   expect(response.body.homeScore).toBe(1);
-  //   expect(response.body.awayScore).toBe(0);
-  // });
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.homePlayer).toBe('Taro');
+    expect(response.body.awayPlayer).toBe('Hanako');
+    expect(response.body.homeScore).toBe(1);
+    expect(response.body.awayScore).toBe(0);
+  });
 
-  // it("case2: POST /match - リクエストボディの指定がないときは400", async () => {
-  //   const response = await request(app)
-  //     .post('/match')
-  //     .send({});
+  it("case2: POST /match - リクエストボディの指定がないときは400", async () => {
+    const response = await request(app)
+      .post('/match')
+      .send({});
 
-  //   expect(response.status).toBe(400);
-  // });
+    expect(response.status).toBe(400);
+  });
 
   it('dummy: GET /matches - 全ての試合が取得できる', async () => {
-    expect(true).toBe(true);
-    // const response = await request(app).get('/matches');
+    const response = await request(app).get('/matches');
 
-    // expect(response.status).toBe(200);
-    // expect(response.body.length).toBeGreaterThan(0);  // 試合が存在することを確認
-    // expect(response.body[0]).toHaveProperty('homePlayer');
+    expect(response.status).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);  // 試合が存在することを確認
+    expect(response.body[0]).toHaveProperty('homePlayer');
   });
 });

5ddcfdc9b7fffc312e1b4efc9484fc38e0f7d414
uhablog <uha.blog98@gmail.com>
2024-10-21 16:59:00 +0900

    actions修正

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,6 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test': '.env'});
 
+console.log({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT)
+});
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,

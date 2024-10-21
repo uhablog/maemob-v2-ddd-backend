12a682879d2e0e6f84210c8109b09c442066e60f
uhablog <uha.blog98@gmail.com>
2024-10-21 17:08:07 +0900

    fix actions

--- db/init.sql
+++ db/init.sql
@@ -1,27 +1,7 @@
--- 開発用データベース
-CREATE DATABASE maemob;
+-- -- テスト用データベース
+-- CREATE DATABASE maemob_test;
 
-\c maemob;
-
-CREATE TABLE players (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  points INT DEFAULT 0
-);
-
-CREATE TABLE matches (
-  id SERIAL PRIMARY KEY,
-  home_player_id INT NOT NULL,
-  away_player_id INT NOT NULL,
-  home_score INT NOT NULL,
-  away_score INT NOT NULL,
-  match_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- テスト用データベース
-CREATE DATABASE maemob_test;
-
-\c maemob_test;
+-- \c maemob_test;
 
 CREATE TABLE players (
   id SERIAL PRIMARY KEY,
--- src/config/db.ts
+++ src/config/db.ts
@@ -5,14 +5,6 @@ dotenv.config({ path: process.env.NODE_ENV === 'test' ? '.env.test': '.env'});
 
 console.log(process.env.DB_NAME?.length);
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: Number(process.env.DB_PORT)
-});
-
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,

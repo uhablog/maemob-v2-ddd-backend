d17684464943e72f645357fb6bc2d60b4fb6fd81
uhablog <uha.blog98@gmail.com>
2024-11-08 11:22:52 +0900

    仕様書・テスト・DBの修正完了

--- db/init.sql
+++ db/init.sql
@@ -13,6 +13,11 @@ CREATE TABLE players (
   convention_id UUID NOT NULL,
   name VARCHAR(50) NOT NULL,
   points INT DEFAULT 0,
+  win INT DEFAULT 0,
+  draw INT DEFAULT 0,
+  lose INT DEFAULT 0,
+  goals INT DEFAULT 0,
+  concede INT DEFAULT 0,
   CONSTRAINT fk_player_conventions FOREIGN KEY (convention_id)
     REFERENCES conventions(id)
 );

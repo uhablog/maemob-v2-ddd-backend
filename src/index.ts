b17471f504bac84ae2fb6fcaba28ef2a104a2b9a
uhablog <uha.blog98@gmail.com>
2025-03-22 13:03:49 +0900

    認可の追加

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ import scorerRouter from './interfaces/http/routes/scorerRouter';
 import assistRouter from './interfaces/http/routes/assistRouter';
 import momRouter from './interfaces/http/routes/momRouter';
 import http from 'http';
+import { expressjwt } from 'express-jwt';
+import JwksRsa from 'jwks-rsa';
+import { unless } from 'express-unless';
 
 const app = express();
 app.use(express.json());
@@ -21,8 +24,39 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']  // 必要なヘッダーを指定
 }));
 
+// 認可用のコードを追加
+const authConfig = {
+  domain: process.env.AUTH0_DOMAIN!,
+  audience: process.env.AUTH0_AUDIENCE!,
+};
+
+const checkJwt = expressjwt({
+  secret: JwksRsa.expressJwtSecret({
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+    cache: true,
+    rateLimit: true,
+  }),
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithms: ['RS256'],
+});
+
+checkJwt.unless = unless;
+
+// テスト時外は認証をする
+if (process.env.NODE_ENV !== 'test') {
+
+  // Swagger UIからのリクエストの場合は認可をしない
+  app.use(checkJwt.unless((req) => {
+    const referer = req.headers.referer || '';
+    return referer.includes('/api-docs');
+  }));
+}
+
 // Swagger UIをセットアップ
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+if (process.env.NODE_ENV !== 'production') {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 app.use('/api', [
   playerRouter,
@@ -33,7 +67,7 @@ app.use('/api', [
   momRouter
 ]);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8888;
 
 let server: http.Server | undefined;
 if (process.env.NODE_ENV !== 'test') {
